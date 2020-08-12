fd718e3a98ad324525e964532cd550a9f9c65dd0
Ulugbekadmin <ueshonqulov652@gmail.com>
2020-08-12 13:54:17 -0700

    html css js

--- common/widgets/views/hero.php
+++ common/widgets/views/hero.php
@@ -10,10 +10,10 @@
               <div class="owl-stage-outer">
                 <div class="owl-stage">
              <div class="owl-item">
-               <div class="hs-item set-bg" data-setbg="img/hero/1.1.jpg" style="background-image: url('assets/img/hero/1.1.jpg');">
+               <div class="hs-item set-bg" data-setbg="img/hero/1.1.jpg">
                 <div class="container">
                     <div class="row">
-                        <div class="text-white">
+                        <div class="col-12 text-truncate text-white">
                               <h2 class="text">Only Natural Materials</h2>
                         </div>
                       </div>
@@ -21,10 +21,10 @@
               </div>
             </div>
             <div class="owl-item active">
-              <div class="hs-item set-bg" data-setbg="img/hero/1.2.jpg" style="background-image: url('assets/img/hero/1.2.jpg');">
+              <div class="hs-item set-bg" data-setbg="img/hero/1.2.jpg">
                 <div class="container">
                   <div class="row">
-                    <div class="col-12 text-white">
+                    <div class="col-12 text-truncatetext-white">
                         <h2 class="text">Delivery to anywhere in the world</h2>
                       </div>
                     </div>
@@ -32,10 +32,10 @@
                 </div>
               </div>
               <div class="owl-item active" >
-                <div class="hs-item set-bg" data-setbg="img/hero/1.3.jpg" style="background-image: url('assets/img/hero/1.3.jpg');">
+                <div class="hs-item set-bg" data-setbg="img/hero/1.3.jpg">
                   <div class="container">
                     <div class="row">
-                      <div class="col-12 text-white">
+                      <div class="col-12 text-truncate text-white">
                         <h2 class="text">Our craftsmen</h2>
                         </div>
                       </div>
@@ -43,10 +43,10 @@
                   </div>
                 </div>
                 <div class="owl-item active" >
-                  <div class="hs-item set-bg" data-setbg="img/hero/1.4.jpg" style="background-image: url('assets/img/hero/1.4.jpg');">
+                  <div class="hs-item set-bg" data-setbg="img/hero/1.4.jpg">
                     <div class="container">
                       <div class="row">
-                        <div class="col-12 text-white">
+                        <div class="col-12 text-truncate text-white">
                             <h2 class="text">free and fast delivery</h2>
                           </div>
                         </div>
@@ -57,11 +57,11 @@
             </div>
           </div>
         </div>
-          <div class="col-12 col-md-3 m-0 p-0-0-0-1 row">
-            <div class="col-6 col-md-12 p-0">
+          <div class="col-12 col-md-3  m-0 pt-5 pt-md-0 mt-4 mt-md-0 row">
+            <div class="col-md-12 col-sm-6 col-12 p-0">
               <img src="img/hero/top/1.jpg" alt="">
             </div>
-            <div class="col-6 col-md-12 p-0">
+            <div class="col-md-12 col-sm-6 col-12 p-0">
               <img src="img/hero/top/2.jpg" alt="">
             </div>
           </div>
